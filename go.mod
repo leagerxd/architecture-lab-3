21053dc39b3fbb6d35e284093b617e283fb8284b
leagerxd <misha.zabotin@gmail.com>
2023-06-26 12:32:07 +0300

    Added realization for task one.
    Yellow T-shape figure, that reacts by clicking on left mouse button.

--- go.mod
+++ go.mod
@@ -4,10 +4,11 @@ go 1.20
 
 require (
 	dmitri.shuralyov.com/gpu/mtl v0.0.0-20221208032759-85de2813cf6b // indirect
-	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20200222043503-6f7a984d4dc4 // indirect
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20200707082815-5321531c36a2 // indirect
+	github.com/hajimehoshi/ebiten v1.12.12 // indirect
 	github.com/jezek/xgb v1.0.0 // indirect
 	golang.org/x/exp/shiny v0.0.0-20230321023759-10a507213a29 // indirect
 	golang.org/x/image v0.7.0 // indirect
-	golang.org/x/mobile v0.0.0-20201217150744-e6ae53a27f4f // indirect
+	golang.org/x/mobile v0.0.0-20210208171126-f462b3930c8f // indirect
 	golang.org/x/sys v0.5.0 // indirect
 )
--- ui/window.go
+++ ui/window.go
@@ -1,142 +1,57 @@
-package ui
+package main
 
 import (
-	"image"
 	"image/color"
 	"log"
 
-	"golang.org/x/exp/shiny/driver"
-	"golang.org/x/exp/shiny/imageutil"
-	"golang.org/x/exp/shiny/screen"
-	"golang.org/x/image/draw"
-	"golang.org/x/mobile/event/key"
-	"golang.org/x/mobile/event/lifecycle"
-	"golang.org/x/mobile/event/mouse"
-	"golang.org/x/mobile/event/paint"
-	"golang.org/x/mobile/event/size"
+	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
-type Visualizer struct {
-	Title         string
-	Debug         bool
-	OnScreenReady func(s screen.Screen)
+const (
+	screenWidth  = 800
+	screenHeight = 800
+	figureWidth  = 100
+	figureHeight = 300
+)
 
-	w    screen.Window
-	tx   chan screen.Texture
-	done chan struct{}
+var (
+	figureX = (screenWidth - figureWidth) / 2
+	figureY = (screenHeight - figureHeight) / 2
+)
 
-	sz  size.Event
-	pos image.Rectangle
-}
+type Game struct{}
 
-func (pw *Visualizer) Main() {
-	pw.tx = make(chan screen.Texture)
-	pw.done = make(chan struct{})
-	pw.pos.Max.X = 200
-	pw.pos.Max.Y = 200
-	driver.Main(pw.run)
-}
-
-func (pw *Visualizer) Update(t screen.Texture) {
-	pw.tx <- t
-}
-
-func (pw *Visualizer) run(s screen.Screen) {
-	w, err := s.NewWindow(&screen.NewWindowOptions{
-		Title: pw.Title,
-	})
-	if err != nil {
-		log.Fatal("Failed to initialize the app window:", err)
+func (g *Game) Update(screen *ebiten.Image) error {
+	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
+		x, y := ebiten.CursorPosition()
+		figureX = x - figureWidth/2
+		figureY = y - figureHeight/2
 	}
-	defer func() {
-		w.Release()
-		close(pw.done)
-	}()
-
-	if pw.OnScreenReady != nil {
-		pw.OnScreenReady(s)
-	}
-
-	pw.w = w
-
-	events := make(chan any)
-	go func() {
-		for {
-			e := w.NextEvent()
-			if pw.Debug {
-				log.Printf("new event: %v", e)
-			}
-			if detectTerminate(e) {
-				close(events)
-				break
-			}
-			events <- e
-		}
-	}()
-
-	var t screen.Texture
-
-	for {
-		select {
-		case e, ok := <-events:
-			if !ok {
-				return
-			}
-			pw.handleEvent(e, t)
-
-		case t = <-pw.tx:
-			w.Send(paint.Event{})
-		}
-	}
+	return nil
 }
 
-func detectTerminate(e any) bool {
-	switch e := e.(type) {
-	case lifecycle.Event:
-		if e.To == lifecycle.StageDead {
-			return true // Window destroy initiated.
-		}
-	case key.Event:
-		if e.Code == key.CodeEscape {
-			return true // Esc pressed.
-		}
-	}
-	return false
+func (g *Game) Draw(screen *ebiten.Image) {
+	screen.Fill(color.Black)
+	drawFigure(screen, figureX, figureY)
 }
 
-func (pw *Visualizer) handleEvent(e any, t screen.Texture) {
-	switch e := e.(type) {
-
-	case size.Event: // Оновлення даних про розмір вікна.
-		pw.sz = e
-
-	case error:
-		log.Printf("ERROR: %s", e)
-
-	case mouse.Event:
-		if t == nil {
-			// TODO: Реалізувати реакцію на натискання кнопки миші.
-		}
-
-	case paint.Event:
-		// Малювання контенту вікна.
-		if t == nil {
-			pw.drawDefaultUI()
-		} else {
-			// Використання текстури отриманої через виклик Update.
-			pw.w.Scale(pw.sz.Bounds(), t, t.Bounds(), draw.Src, nil)
-		}
-		pw.w.Publish()
-	}
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return screenWidth, screenHeight
 }
 
-func (pw *Visualizer) drawDefaultUI() {
-	pw.w.Fill(pw.sz.Bounds(), color.Black, draw.Src) // Фон.
+func drawFigure(screen *ebiten.Image, x, y int) {
+	figureColor := color.RGBA{255, 255, 0, 255}
+	ebitenutil.DrawRect(screen, float64(x), float64(y), float64(figureWidth), float64(figureHeight), figureColor)
+	ebitenutil.DrawRect(screen, float64(x), float64(y+figureHeight/2-figureWidth/2), float64(figureWidth), float64(figureHeight), figureColor)
+	ebitenutil.DrawRect(screen, float64(x+figureWidth/2-figureHeight/2), float64(y), float64(figureHeight), float64(figureWidth), figureColor)
+}
 
-	// TODO: Змінити колір фону та додати відображення фігури у вашому варіанті.
-
-	// Малювання білої рамки.
-	for _, br := range imageutil.Border(pw.sz.Bounds(), 10) {
-		pw.w.Fill(br, color.White, draw.Src)
+func main() {
+	ebiten.SetWindowSize(screenWidth, screenHeight)
+	ebiten.SetWindowTitle("Painter")
+	game := &Game{}
+	if err := ebiten.RunGame(game); err != nil {
+		log.Fatal(err)
 	}
 }
