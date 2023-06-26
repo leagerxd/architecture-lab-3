d4c64fe8958d489d4d33ab4a322925d1fc394d23
Sviat Oleksyn <sviat1001@gmail.com>
2023-06-26 16:17:31 +0300

    Added op.go: command logic

--- painter/op.go
+++ painter/op.go
@@ -1,9 +1,11 @@
 package painter
 
 import (
+	"image"
 	"image/color"
 
 	"golang.org/x/exp/shiny/screen"
+	"golang.org/x/image/draw"
 )
 
 // Operation змінює вхідну текстуру.
@@ -12,7 +14,7 @@ type Operation interface {
 	Do(t screen.Texture) (ready bool)
 }
 
-// OperationList групує список операцій в одну.
+// OperationList групує список операції в одну.
 type OperationList []Operation
 
 func (ol OperationList) Do(t screen.Texture) (ready bool) {
@@ -37,12 +39,49 @@ func (f OperationFunc) Do(t screen.Texture) bool {
 	return false
 }
 
-// WhiteFill зафарбовує текстуру у білий колір. Може бути використана як Operation через OperationFunc(WhiteFill).
-var WhiteFill OperationFunc = func(t screen.Texture) {
+// WhiteFill зафарбовує тестуру у білий колір. Може бути викоистана як Operation через OperationFunc(WhiteFill).
+func WhiteFill(t screen.Texture) {
 	t.Fill(t.Bounds(), color.White, screen.Src)
 }
 
-// GreenFill зафарбовує текстуру у зелений колір. Може бути використана як Operation через OperationFunc(GreenFill).
-var GreenFill OperationFunc = func(t screen.Texture) {
+// GreenFill зафарбовує тестуру у зелений колір. Може бути викоистана як Operation через OperationFunc(GreenFill).
+func GreenFill(t screen.Texture) {
 	t.Fill(t.Bounds(), color.RGBA{G: 0xff, A: 0xff}, screen.Src)
+}
+
+type BgRectangle struct {
+	X1, Y1, X2, Y2 int
+}
+
+func (op *BgRectangle) Do(t screen.Texture) bool {
+	t.Fill(image.Rect(op.X1, op.Y1, op.X2, op.Y2), color.Black, screen.Src)
+	return false
+}
+
+type Figure struct {
+	X, Y int
+	C    color.RGBA
+}
+
+func (op *Figure) Do(t screen.Texture) bool {
+	t.Fill(image.Rect(op.X-150, op.Y-100, op.X+150, op.Y), op.C, draw.Src)
+	t.Fill(image.Rect(op.X-50, op.Y, op.X+50, op.Y+100), op.C, draw.Src)
+	return false
+}
+
+type Move struct {
+	X, Y    int
+	Figures []*Figure
+}
+
+func (op *Move) Do(t screen.Texture) bool {
+	for i := range op.Figures {
+		op.Figures[i].X += op.X
+		op.Figures[i].Y += op.Y
+	}
+	return false
+}
+
+func ResetScreen(t screen.Texture) {
+	t.Fill(t.Bounds(), color.Black, draw.Src)
 }
